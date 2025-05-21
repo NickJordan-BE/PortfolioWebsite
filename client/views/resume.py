a5a6538feb068a2a449c61daf8cd2118c9a63351
njordan <njordan1@uw.edu>
2025-05-20 17:08:50 -0700

    Fix images

--- client/views/resume.py
+++ client/views/resume.py
@@ -1,12 +1,13 @@
 import streamlit as st
-import base64
-import requests
 
 st.title("My Resume")
 
+file_id = "1Qx8h8Xk7lofHnyGyb6IlRQefgk_TxCuQ"  # Replace with your actual file ID
 
-# Use iframe instead of embed
-pdf_display = f"""
-<iframe src="https://drive.google.com/file/d/1Qx8h8Xk7lofHnyGyb6IlRQefgk_TxCuQ/view?usp=sharing" width="700" height="1000" type="application/pdf"></iframe>
-"""
-st.markdown(pdf_display, unsafe_allow_html=True)
+embed_url = f"https://drive.google.com/file/d/{file_id}/preview"
+
+st.markdown(
+    f'<iframe src="{embed_url}" width="700" height="1000" allow="autoplay"></iframe>',
+    unsafe_allow_html=True
+)
+
